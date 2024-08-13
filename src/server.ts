ab27d5d7a295165cc092b5f9ddf94bbb1fe69534
Eduardo Ernesto Corrales Reyna <desenvolvimento1@credencieconsultas.com.br>
2024-08-13 00:38:16 -0300

    feat: add first route

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import fastify from 'fastify';
 
 const app = fastify();
 
+app.get('/', () => {
+  return 'Everything OK!';
+});
+
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running on port 3333');
 });
