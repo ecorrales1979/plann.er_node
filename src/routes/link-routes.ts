74b39c8edfbe9616205673a7f55064d3247bbb1a
Eduardo Ernesto Corrales Reyna <desenvolvimento1@credencieconsultas.com.br>
2024-08-19 00:08:44 -0300

    feat: add route to create trip links

---
+++ src/routes/link-routes.ts
@@ -0,0 +1,39 @@
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { prisma } from '../lib/prisma';
+
+const paramsSchema = z.object({
+  tripId: z.string().uuid(),
+});
+
+const bodySchema = z.object({
+  title: z.string().min(4),
+  url: z.string().url(),
+});
+
+type Params = z.infer<typeof paramsSchema>;
+
+export async function linkRoutes(app: FastifyInstance) {
+  app.post<{ Params: Params }>('/trips/:tripId/links', async (request) => {
+    const { tripId } = paramsSchema.parse(request.params);
+    const { url, title } = bodySchema.parse(request.body);
+
+    const trip = await prisma.trip.findUnique({
+      where: { id: tripId },
+    });
+
+    if (!trip) {
+      throw new Error('Trip not found');
+    }
+
+    const link = await prisma.link.create({
+      data: {
+        title,
+        url,
+        trip_id: tripId,
+      },
+    });
+
+    return { linkId: link.id };
+  });
+}
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import fastify from 'fastify';
 import { activityRoutes } from './routes/activity-routes';
 import { confirmParticipant } from './routes/confirm-participant';
 import { confirmTrip } from './routes/confirm-trip';
+import { linkRoutes } from './routes/link-routes';
 import { tripRoutes } from './routes/trip-routes';
 
 const app = fastify();
@@ -19,6 +20,7 @@ app.register(tripRoutes);
 app.register(confirmTrip);
 app.register(confirmParticipant);
 app.register(activityRoutes);
+app.register(linkRoutes);
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running on port 3333');
