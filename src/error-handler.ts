b142fc3af9a5c10a28d7a500d04fd0c48336b5b6
Eduardo Ernesto Corrales Reyna <desenvolvimento1@credencieconsultas.com.br>
2024-08-27 00:05:22 -0300

    feat: handle zod validation errors

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { ZodError } from 'zod';
 import { ClientError } from './errors/client-error';
 
 type FastifyErrorHandler = FastifyInstance['errorHandler'];
@@ -8,6 +9,12 @@ export const errorHandler: FastifyErrorHandler = (
   request: FastifyRequest,
   reply: FastifyReply
 ) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(409)
+      .send({ messgae: 'Invalid inputs', errors: error.flatten().fieldErrors });
+  }
+
   if (error instanceof ClientError) {
     return reply.status(error.code).send({ message: error.message });
   }
