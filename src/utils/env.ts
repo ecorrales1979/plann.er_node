c824cc9a3f5bcb05419373e037526fded4b76526
Eduardo Ernesto Corrales Reyna <desenvolvimento1@credencieconsultas.com.br>
2024-08-29 23:41:27 -0300

    feat: add front_url to env validation

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
   SERVER_URL: z.string().url(),
+  FRONT_URL: z.string().url(),
   PORT: z.coerce.number().default(3333),
 });
 
